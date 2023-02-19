0d1c24e9b0ac26b73c01d342f9a3cd4e6f9539d1
Kirill <freestyle.ff50@gmail.com>
2023-02-19 22:38:03 +0700

    initial commit

--- TFYAiK/SaveFile.cs
+++ TFYAiK/SaveFile.cs
@@ -6,72 +6,49 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace TFYAiK
 {
     public class SaveFile
     {
-        private string currentName;
-        private string currentDirectory;
+        private string currentFile;
         public SaveFile()
         {
-            this.currentDirectory = null; this.currentName = null;
+            this.currentFile = null;
         }
 
         public SaveFile(string name)
         {
-            this.currentName = name;
+            this.currentFile = name;
         }
-        
+
         public void SaveAs(string text)
         {
-            if (currentName == null)
-            {
-                FileStream myStream;
-                //вызов окна выбора имени
-                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
-                saveFileDialog1.Filter = "Text |*.txt|All files (*.*)|*.*";
-                saveFileDialog1.Title = "Save an Text File";
-                saveFileDialog1.RestoreDirectory = true;
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text |*.txt|All files (*.*)|*.*";
+            saveFileDialog.Title = "Save an Text File";
+            saveFileDialog.RestoreDirectory = true; ;
 
-                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
-                {
-                    if ((myStream = (FileStream)saveFileDialog1.OpenFile()) != null)
-                    {
-                        byte[] bytes = Encoding.ASCII.GetBytes(text);
-                        myStream.Write(bytes, 0, text.Length);
-                        currentDirectory = myStream.Name;
-                        saveFileDialog1.FileName = currentName;
-                        myStream.Close();
-                    }
-                }
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter streamWriter = new StreamWriter(saveFileDialog.FileName);
+                streamWriter.WriteLine(text);
+                streamWriter.Close();
             }
         }
 
-        public void Save(string text) 
+        public void Save(string text)
         {
-            if (currentName == null)
+            if (currentFile == null)
             {
-                Stream myStream;
-                //вызов окна выбора имени
-                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
-                saveFileDialog1.Filter = "Text |*.txt|All files (*.*)|*.*";
-                saveFileDialog1.Title = "Save an Text File";
-                saveFileDialog1.RestoreDirectory = true;
-
-                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
-                {
-                    if ((myStream = saveFileDialog1.OpenFile()) != null)
-                    {
-                        byte[] bytes = Encoding.ASCII.GetBytes(text);
-                        myStream.Write(bytes, 0, text.Length);
-                        myStream.Close();
-                    }
-                }               
+                SaveAs(text);
             }
             else
             {
-                StreamWriter myStream = new StreamWriter(currentDirectory + currentName, false, System.Text.Encoding.ASCII);            
+                StreamWriter streamWriter = new StreamWriter(currentFile);
+                streamWriter.WriteLine(text);
+                streamWriter.Close();
             }
 
         }
