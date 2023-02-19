c3744878208e489bfaf78e24cdad8cf17c5fab77
Kirill <freestyle.ff50@gmail.com>
2023-02-19 22:38:03 +0700

    initial commit

--- TFYAiK/SaveFile.cs
+++ TFYAiK/SaveFile.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace TFYAiK
 {
@@ -22,7 +23,7 @@ namespace TFYAiK
         {
             this.currentName = name;
         }
-        
+
         public void SaveAs(string text)
         {
             if (currentName == null)
@@ -48,30 +49,25 @@ namespace TFYAiK
             }
         }
 
-        public void Save(string text) 
+        public void Save(string text)
         {
             if (currentName == null)
             {
-                Stream myStream;
-                //вызов окна выбора имени
-                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
-                saveFileDialog1.Filter = "Text |*.txt|All files (*.*)|*.*";
-                saveFileDialog1.Title = "Save an Text File";
-                saveFileDialog1.RestoreDirectory = true;
+                SaveFileDialog saveFileDialog = new SaveFileDialog(); 
+                saveFileDialog.Filter = "Text |*.txt|All files (*.*)|*.*";
+                saveFileDialog.Title = "Save an Text File";
+                saveFileDialog.RestoreDirectory = true; ;
 
-                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
                 {
-                    if ((myStream = saveFileDialog1.OpenFile()) != null)
-                    {
-                        byte[] bytes = Encoding.ASCII.GetBytes(text);
-                        myStream.Write(bytes, 0, text.Length);
-                        myStream.Close();
-                    }
-                }               
+                    StreamWriter streamWriter = new StreamWriter(saveFileDialog.FileName);
+                    streamWriter.WriteLine(text);
+                    streamWriter.Close();
+                }
             }
             else
             {
-                StreamWriter myStream = new StreamWriter(currentDirectory + currentName, false, System.Text.Encoding.ASCII);            
+                StreamWriter myStream = new StreamWriter(currentDirectory + currentName, false, System.Text.Encoding.ASCII);
             }
 
         }
